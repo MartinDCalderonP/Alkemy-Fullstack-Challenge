0ccb02bc419c6d07584f9b9df3b962469d30c0f0
Martín Calderón <martincalderon92@outlook.com>
2022-01-05 20:11:08 -0300

    Add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Navbar.module.scss';
+
+export default function Navbar() {
+	return (
+		<nav className={styles.navbar}>
+			<p>Alkemy Fullstack Challenge</p>
+		</nav>
+	);
+}
