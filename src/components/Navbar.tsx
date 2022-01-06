1e3a4523090080faf6a1aaba9db48974e3cb4398
Martín Calderón <martincalderon92@outlook.com>
2022-01-06 12:37:44 -0300

    Uses Material UI in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.scss';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 export default function Navbar() {
 	return (
-		<nav className={styles.navbar}>
-			<p>Alkemy Fullstack Challenge</p>
-		</nav>
+		<AppBar position="static" color="inherit">
+			<Toolbar className={styles.navbar}>
+				<Typography variant="h6">MERN ToDo App</Typography>
+			</Toolbar>
+		</AppBar>
 	);
 }
