3f0a293b6c50514e142837d6afcb9b07ad0ed5b0
Martín Calderón <martincalderon92@outlook.com>
2022-01-07 20:29:37 -0300

    Create basic Table structure

--- src/common/interfaces.tsx
+++ src/common/interfaces.tsx
@@ -1,4 +1,12 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react';
+
+export interface IMove {
+	move_id: number;
+	move_description: string;
+	move_amount: number;
+	move_type: string;
+	move_date: Date;
+}
 
 export interface IToastProps {
 	message: string;
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Table.module.scss';
+import { API } from '../common/Enums';
+import { capitalizeWord, format } from '../common/Helpers';
+import Toast from './Toast';
+import { IMove } from '../common/Interfaces';
 
 export default function Table() {
+	const fetchUrl = `${API.base}${API.moves}`;
+	const [moves, setMoves] = useState([]);
+	const [message, setMessage] = useState('');
+
+	const getMoves = () => {
+		fetch(fetchUrl)
+			.then((res) => res.json())
+			.then((data) => setMoves(data))
+			.catch((err) => setMessage(err));
+	};
+
+	useEffect(() => {
+		getMoves();
+	}, []);
+
+	const handleCloseToast = () => {
+		setMessage('');
+	};
+
 	return (
-		<table className={styles.table}>
-			<thead>
-				<tr>
-					<th>Description</th>
-					<th>Amount</th>
-					<th>Type</th>
-					<th>Date</th>
-				</tr>
-			</thead>
-			<tbody />
-		</table>
+		<>
+			<div className={styles.container}>
+				<table>
+					<thead>
+						<tr>
+							<th>Description</th>
+							<th>Amount</th>
+							<th>Type</th>
+							<th>Date</th>
+							<th>Edit</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						{moves.map((move: IMove) => (
+							<tr key={move.move_id}>
+								<td>{move.move_description}</td>
+								<td>{'$ ' + move.move_amount}</td>
+								<td>{capitalizeWord(move.move_type)}</td>
+								<td>{format(move.move_date, 'es')}</td>
+								<td>
+									<button>Edit</button>
+								</td>
+								<td>
+									<button>Delete</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+			{message && <Toast message={message} closeToast={handleCloseToast} />}
+		</>
 	);
 }
