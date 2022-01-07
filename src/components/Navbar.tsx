6974b379c943de65a5bafc05eef84265c1e8c1a9
Martín Calderón <martincalderon92@outlook.com>
2022-01-07 15:40:39 -0300

    Simplify Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.scss';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
 export default function Navbar() {
-	return (
-		<AppBar position="static" color="inherit">
-			<Toolbar className={styles.navbar}>
-				<Typography variant="h6">MERN ToDo App</Typography>
-			</Toolbar>
-		</AppBar>
-	);
+	return <nav className={styles.navbar}>Alkemy Fullstack Challenge</nav>;
 }
