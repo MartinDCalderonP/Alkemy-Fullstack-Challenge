c8131d444bfccd7bb847827808ebc02fe4f64d31
Martín Calderón <martincalderon92@outlook.com>
2022-01-08 20:53:09 -0300

    Add logic to pass from one component to other

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 import TablesContainer from './components/TablesContainer';
 import Footer from './components/Footer';
 
 export default function App() {
+	const [getMove, setGetMove] = useState(0);
+	const [refreshMoves, setRefreshMoves] = useState(0);
+
+	const onGetMove = (moveId: number) => {
+		setGetMove(moveId);
+	};
+
+	const onRefreshMoves = () => {
+		setRefreshMoves(refreshMoves + 1);
+	};
+
 	return (
 		<div>
 			<Navbar />
-			<Form />
-			<TablesContainer />
+			<Form getMove={getMove} refreshMoves={onRefreshMoves} />
+			<TablesContainer getMove={onGetMove} refreshMoves={refreshMoves} />
 			<Footer />
 		</div>
 	);
