3e84ac1efaf1bbb3af74037d22e4268cae00ae31
Martín Calderón <martincalderon92@outlook.com>
2022-01-09 20:48:39 -0300

    Add Home icon on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
-	return <nav className={styles.navbar}>Alkemy Fullstack Challenge</nav>;
+		<nav className={styles.navbar}>
+			<a href="#" className={styles.homeAnchor}>
+				<FontAwesomeIcon className={styles.homeIcon} icon={faHome} />
+				Home
+			</a>
 }
