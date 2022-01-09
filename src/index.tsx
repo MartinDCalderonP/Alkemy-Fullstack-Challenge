7e6dae67b20761543b47734b96c2b6bc907a8b3c
Martín Calderón <martincalderon92@outlook.com>
2022-01-09 20:26:40 -0300

    Add StateProvider to Index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { StateProvider } from './context/Context';
 import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<StateProvider>
+			<App />
+		</StateProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
