454ae106a87ddab3d7f8fdab0c96524dd1919c91
Martín Calderón <martincalderon92@outlook.com>
2022-01-09 21:04:42 -0300

    Delete multiline prop on description input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -139,7 +139,6 @@ export default function Form({ getMove, refreshMoves }: IFormProps) {
 							name="description"
 							value={move.description}
 							onChange={handleChange}
-							multiline
 						/>
 
 						<TextField
