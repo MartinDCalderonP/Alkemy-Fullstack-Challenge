ed2b86dc07653b24c2e54c0994910f9fb5b5576d
Martín Calderón <martincalderon92@outlook.com>
2022-01-09 21:03:15 -0300

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.scss';
+import SignButtons from './SignButtons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
+	return (
 		<nav className={styles.navbar}>
 			<a href="#" className={styles.homeAnchor}>
 				<FontAwesomeIcon className={styles.homeIcon} icon={faHome} />
 				Home
 			</a>
+
+			<h1>Alkemy Fullstack Challenge</h1>
+
+			<SignButtons />
+		</nav>
+	);
 }
