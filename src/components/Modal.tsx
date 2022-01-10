aa33caf3921c3ac1b3d1052e3156fea5fa722b22
Martín Calderón <martincalderon92@outlook.com>
2022-01-09 23:26:16 -0300

    Fix some redundances

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,35 +1,19 @@
-import React, { useState, useEffect, ChangeEvent, MouseEvent } from 'react';
+import React, { useState, ChangeEvent, MouseEvent } from 'react';
 import { createPortal } from 'react-dom';
 import styles from '../styles/Modal.module.scss';
 import { API } from '../common/Enums';
 import { IModalProps, IUserData } from '../common/Interfaces';
-import { useContextState } from '../context/Context';
-import { actionTypes, initialState } from '../context/Reducer';
+import { initialState } from '../context/Reducer';
 import useLocalStorage from '../hooks/useLocalStorage';
 import CloseIcon from './CloseIcon';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 export default function Modal({ closeModal }: IModalProps) {
-	const { state, dispatch } = useContextState();
-
-	const [userInStorage, setUserInStorage] = useLocalStorage<IUserData>(
+	const [, setUserInStorage] = useLocalStorage<IUserData>(
 		'user',
 		initialState.user
 	);
-
-	useEffect(() => {
-		if (userInStorage.user_id) {
-			dispatch({
-				type: actionTypes.SET_USER,
-				payload: { user: userInStorage },
-			});
-		}
-
-		if (state.user.user_id) {
-			closeModal();
-		}
-	}, [userInStorage.user_id]);
 
 	const [userToSignIn, setUserToSignIn] = useState({
 		email: '',
@@ -71,10 +55,6 @@ export default function Modal({ closeModal }: IModalProps) {
 					setMessage(data.message);
 				} else {
 					setUserInStorage(data.user);
-					dispatch({
-						type: actionTypes.SET_USER,
-						payload: { user: userInStorage },
-					});
 					closeModal();
 				}
 			});
