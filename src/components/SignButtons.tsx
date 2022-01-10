c8dd66b8002dc5c86a8d4ccbbaac0b790e3fd60a
Martín Calderón <martincalderon92@outlook.com>
2022-01-09 21:03:05 -0300

    Create and import sign buttons

---
+++ src/components/SignButtons.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import styles from '../styles/SignButtons.module.scss';
+import { useContextState } from '../context/Context';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faSignInAlt,
+	faSignOutAlt,
+	faUserCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import Modal from './Modal';
+
+export default function SignButtons() {
+	const { state, dispatch } = useContextState();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	console.log(state, dispatch);
+
+	const handleOpenModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const handleSignOut = () => {
+		console.log('Sign out');
+	};
+
+	return (
+		<>
+			{state?.user?.user_id === 0 && (
+				<button className={styles.signButton} onClick={handleOpenModal}>
+					Sign In
+					<FontAwesomeIcon className={styles.anchorIcon} icon={faSignInAlt} />
+				</button>
+			)}
+
+			{state?.user?.user_id > 0 && (
+				<>
+					<div className={styles.profileButton}>
+						<FontAwesomeIcon
+							className={styles.anchorIcon}
+							icon={faUserCircle}
+						/>
+					</div>
+
+					<button className={styles.signButton} onClick={handleSignOut}>
+						<FontAwesomeIcon
+							className={styles.anchorIcon}
+							icon={faSignOutAlt}
+						/>
+					</button>
+				</>
+			)}
+
+			{isModalOpen && <Modal closeModal={() => setIsModalOpen(false)} />}
+		</>
+	);
+}
