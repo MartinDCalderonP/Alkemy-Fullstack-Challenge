8cdcb5a9d4c14a1705f2f3c85e1809f80ad7fa96
Martín Calderón <martincalderon92@outlook.com>
2022-01-13 20:11:10 -0300

    Refactor app with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import Form from './components/Form';
-import TablesContainer from './components/TablesContainer';
-import Footer from './components/Footer';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Paths } from './common/Enums';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
 
 export default function App() {
-	const [getMove, setGetMove] = useState(0);
-	const [refreshMoves, setRefreshMoves] = useState(0);
-
-	const onGetMove = (moveId: number) => {
-		setGetMove(moveId);
-	};
-
-	const onRefreshMoves = () => {
-		setRefreshMoves(refreshMoves + 1);
-	};
-
 	return (
-		<div>
-			<Navbar />
-			<Form getMove={getMove} refreshMoves={onRefreshMoves} />
-			<TablesContainer getMove={onGetMove} refreshMoves={refreshMoves} />
-			<Footer />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path={Paths.home} element={<Home />} />
+
+				<Route path={Paths.register} element={<SignUp />} />
+
+				<Route path="*" element={<Navigate replace to={Paths.home} />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
