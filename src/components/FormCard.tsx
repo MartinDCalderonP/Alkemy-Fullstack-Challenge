a62423667c22decc7c0276e7498ecfcb2019bdd5
Martín Calderón <martincalderon92@outlook.com>
2022-01-16 20:00:48 -0300

    Change name from Card to FormCard and add some styles properties

--- src/components/FormCard.tsx
+++ src/components/FormCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import styles from '../styles/Card.module.scss';
+import styles from '../styles/FormCard.module.scss';
 import { ICardProps } from '../common/Interfaces';
 
-export default function Card({ children }: ICardProps) {
+export default function FormCard({ children }: ICardProps) {
 	return <div className={styles.card}>{children}</div>;
 }
