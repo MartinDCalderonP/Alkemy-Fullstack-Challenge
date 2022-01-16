23f0132f20ab4fea5dc9647359bbe552dc16b1d5
Martín Calderón <martincalderon92@outlook.com>
2022-01-16 19:50:09 -0300

    Create SignUp page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import styles from '../styles/SignUp.module.scss';
 import Layout from '../components/Layout';
+import Card from '../components/Card';
+import SignForm from '../components/SignForm';
 
 export default function SignUp() {
 	return (
 		<Layout>
-			<h1>Sign Up</h1>
+			<div className={styles.container}>
+				<Card>
+					<SignForm />
+				</Card>
+			</div>
 		</Layout>
 	);
 }
