2a5d9f4adffaafd1d039bddd205f9a79ceac35ea
Martín Calderón <martincalderon92@outlook.com>
2022-01-16 20:00:58 -0300

    Update SignUp page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import styles from '../styles/SignUp.module.scss';
 import Layout from '../components/Layout';
-import Card from '../components/Card';
+import FormCard from '../components/FormCard';
 import SignForm from '../components/SignForm';
 
 export default function SignUp() {
 	return (
 		<Layout>
-			<div className={styles.container}>
-				<Card>
-					<SignForm />
-				</Card>
-			</div>
+			<FormCard>
+				<SignForm />
+			</FormCard>
 		</Layout>
 	);
 }
