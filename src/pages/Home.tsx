0e4c83b75a0620d23d156869d3d11d7c62cd6c09
Martín Calderón <martincalderon92@outlook.com>
2022-01-17 19:39:02 -0300

    Check if is authenticated to show tables

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import { useContextState } from '../context/Context';
 import Layout from '../components/Layout';
 import MovesForm from '../components/MovesForm';
 import TablesContainer from '../components/TablesContainer';
 
 export default function Home() {
+	const { user } = useContextState();
+
+	const isAuthenticated = user.user_id > 0 ? true : false;
+
 	const [getMove, setGetMove] = useState(0);
 	const [refreshMoves, setRefreshMoves] = useState(0);
 
@@ -18,7 +23,10 @@ export default function Home() {
 	return (
 		<Layout>
 			<MovesForm getMove={getMove} refreshMoves={onRefreshMoves} />
-			<TablesContainer getMove={onGetMove} refreshMoves={refreshMoves} />
+
+			{isAuthenticated && (
+				<TablesContainer getMove={onGetMove} refreshMoves={refreshMoves} />
+			)}
 		</Layout>
 	);
 }
