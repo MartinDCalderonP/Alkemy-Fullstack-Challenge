9765f1b9c64641e1b42c552786c117f0e3cc5192
Martín Calderón <martincalderon92@outlook.com>
2022-01-17 22:46:06 -0300

    Refactor Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,17 @@
-import React, { useState } from 'react';
-import { useContextState } from '../context/Context';
+import React from 'react';
+import styles from '../styles/Home.module.scss';
 import Layout from '../components/Layout';
-import MovesForm from '../components/MovesForm';
-import TablesContainer from '../components/TablesContainer';
 
 export default function Home() {
-	const { user } = useContextState();
-
-	const isAuthenticated = user.user_id > 0 ? true : false;
-
-	const [getMoveById, setGetMove] = useState(0);
-	const [refreshMoves, setRefreshMoves] = useState(0);
-
-	const onGetMove = (moveId: number) => {
-		setGetMove(moveId);
-	};
-
-	const onRefreshMoves = () => {
-		setRefreshMoves(refreshMoves + 1);
-	};
-
 	return (
 		<Layout>
-			<MovesForm getMoveById={getMoveById} refreshMoves={onRefreshMoves} />
-
-			{isAuthenticated && (
-				<TablesContainer getMoveById={onGetMove} refreshMoves={refreshMoves} />
-			)}
+			<div className={styles.home}>
+				<h1 className={styles.title}>Home</h1>
+				<p className={styles.welcome}>
+					Welcome to the home page. You must be logged in to view your moves in
+					the moves page.
+				</p>
+			</div>
 		</Layout>
 	);
 }
