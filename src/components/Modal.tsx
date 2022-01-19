fc26939e6cabb330e4540a47d9ef81db395f9c88
Martín Calderón <martincalderon92@outlook.com>
2022-01-19 19:22:41 -0300

    Add close on click outside in Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { createPortal } from 'react-dom';
 import styles from '../styles/Modal.module.scss';
+import useOnClickOutside from '../hooks/useOnClickOutside';
 import { IModalProps } from '../common/Interfaces';
 import CloseIcon from './CloseIcon';
 import SignForm from './SignForm';
 
 export default function Modal({ toggleModal }: IModalProps) {
+	const ref = useRef(null);
+
 	const handleToggleModal = () => {
-		toggleModal(true);
+		toggleModal();
 	};
+
+	useOnClickOutside(ref, handleToggleModal);
 
 	return createPortal(
 		<div className={styles.overlay}>
-			<div className={`${styles.modal} ${styles.appearModal}`}>
+			<div className={`${styles.modal} ${styles.appearModal}`} ref={ref}>
 				<CloseIcon className={styles.closeIcon} onClick={handleToggleModal} />
 
 				<SignForm toggleModal={handleToggleModal} type="signIn" />
