9fe78a36c9b1dcf1e2537cec35e3ee8ba1a797e6
Martín Calderón <martincalderon92@outlook.com>
2022-01-20 15:48:54 -0300

    Create useFetch hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+export default function useFetch<T>(url: string, options: RequestInit) {
+	const [data, setData] = useState<T | undefined>(undefined);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string | undefined>(undefined);
+
+	const fetchData = async (url: string, options: RequestInit) => {
+		setLoading(true);
+
+		if (!url) {
+			setLoading(false);
+			return;
+		}
+
+		fetch(url, options)
+			.then((res) => res.json())
+			.then((data) => {
+				setData(data);
+				setLoading(false);
+			})
+
+			.catch((err) => {
+				setError(err.message);
+				setLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		const abortController = new AbortController();
+		const signal = abortController.signal;
+
+		const optionsWithSignal: RequestInit = {
+			...options,
+			signal,
+		};
+
+		fetchData(url, optionsWithSignal);
+
+		return () => {
+			abortController.abort();
+		};
+	}, [url, options]);
+
+	return { data, loading, error };
+}
