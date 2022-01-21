19c1fffbc1b8321cb03f555286c59b59b2255359
Martín Calderón <martincalderon92@outlook.com>
2022-01-20 21:56:25 -0300

    Refactor Dropdown to use the useFetch hook

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,11 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from '../styles/Dropdown.module.scss';
 import { Link } from 'react-router-dom';
 import { useContextState } from '../context/Context';
 import { initialUser } from '../context/Reducer';
+import useFetch from '../hooks/useFetch';
 import useOnClickOutside from '../hooks/useOnClickOutside';
 import { Paths } from '../common/Enums';
 import { signOutFetchUrl } from '../common/Helpers';
+import { IStatusResponse } from '../common/Interfaces';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExchangeAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import Swal from 'sweetalert2';
@@ -17,44 +19,46 @@ interface IDropdownProps {
 export default function Dropdown({ toggleDropdown }: IDropdownProps) {
 	const ref = useRef(null);
 
-	const { dispatch } = useContextState();
-
 	const handleToggleDropdown = () => {
 		toggleDropdown();
 	};
 
 	useOnClickOutside(ref, handleToggleDropdown);
 
-	const handleSignOut = () => {
-		const fetchUrl = signOutFetchUrl();
+	const { dispatch } = useContextState();
+	const [fetchUrl, setFetchUrl] = useState('');
+	const [fetchOptions, setFetchOptions] = useState({});
+	const { data, error } = useFetch<IStatusResponse>(fetchUrl, fetchOptions);
 
-		fetch(fetchUrl, {
+	const handleSignOut = () => {
+		setFetchOptions({
 			method: 'DELETE',
-			credentials: 'include',
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.status === 'Success') {
-					dispatch({
-						type: 'SET_USER',
-						payload: {
-							...initialUser,
-						},
-					});
-					Swal.fire({
-						text: data.message,
-						icon: 'success',
-						confirmButtonColor: 'darkblue',
-					});
-				} else {
-					Swal.fire({
-						text: data.message,
-						icon: 'error',
-						confirmButtonColor: 'darkblue',
-					});
-				}
-			});
+			crential: 'include',
+		});
+		setFetchUrl(signOutFetchUrl());
 	};
+
+	useEffect(() => {
+		if (data) {
+			dispatch({
+				type: 'SET_USER',
+				payload: initialUser,
+			});
+			Swal.fire({
+				text: data.message,
+				icon: 'success',
+				confirmButtonColor: 'darkblue',
+			});
+		}
+
+		if (error) {
+			Swal.fire({
+				text: error.message,
+				icon: 'error',
+				confirmButtonColor: 'darkblue',
+			});
+		}
+	}, [data, error]);
 
 	return (
 		<div className={styles.dropdown} ref={ref}>
